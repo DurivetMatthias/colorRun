45611e1bbda79d0d1a9e5715a70076e1e04d7787
BruynoogheMatthias <matthias.bruynooghe@student.howest.be>
2018-05-16 11:36:50 +0200

    arduino0 has rgb and potentio installed

--- Arduino/Arduino0/Arduino0.ino
+++ Arduino/Arduino0/Arduino0.ino
@@ -1,15 +1,80 @@
 #include <Wire.h>
 
-Potentio
+//this one has the bluetooth on 0 and 1
+
+int potentio = A0;
+int RGBred =9;
+int RGBblue=10;
+int RGBgreen=11;
+int piezo;
+
+int potenwaarde=0;
 
 
 
 void setup() {
   // put your setup code here, to run once:
+pinMode(potentio, INPUT);
+pinMode(RGBred, OUTPUT);
+pinMode(RGBblue, OUTPUT);
+pinMode(RGBgreen, OUTPUT);
+pinMode(piezo, OUTPUT);
+
+  
+
+
+Serial.begin(9600);
+
+  //  Serial.begin(38400); // Default communication rate of the Bluetooth module
+  //  Serial.begin(9600); // Default communication rate of the USB module
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  
+  potenwaarde = analogRead(potentio);
+  delay(10);
+  char color = processpotent(potenwaarde);
+  Serial.print(color);
+
+
+
+}
+
+
+char processpotent(int potentwaarde){
+  int range = 1024;
+  if (potentwaarde<(range*0.1)){
+    setColor(0,0,255);
+    return('B');
+  }
+    if (potentwaarde<(range*0.5)){
+    setColor(0,255,0);
+    return('G');
+  }
+  if (potentwaarde<(range*0.9)){
+    setColor(144,255,0);
+    return('Y');
+
+  }
+  if (potentwaarde<range){
+    setColor(255,0,0);
+    return('R');
+  }
+  };
+
+  
+
+
+  void setColor(int red, int green, int blue)
+{
+  analogWrite(RGBred, red);
+  analogWrite(RGBgreen, green);
+  analogWrite(RGBblue, blue);  
+}
+
+void receiveEvent(int bytes) { 
+   // x = Wire.read();
 }
--- Arduino/Arduino1/Arduino1.ino
+++ Arduino/Arduino1/Arduino1.ino
@@ -1,14 +1,68 @@
+#include <LiquidCrystal.h>
+
+#include <Servo.h>
+
 #include <Wire.h>
 
+int button;
+int tilt;
+Servo servo.attach();
 
 
+volatile byte state = LOW;
+
+unsigned long previousMillis = 0; //storing the milliseconds from the previous interrupt
+const long interval = 60000;      //storing the milliseconds between the interval of interrupts
 
 void setup() {
   // put your setup code here, to run once:
 
+
+  pinMode(ledPin, OUTPUT);
+  
+  pinMode(button, INPUT);
+  attachInterrupt(button, buttonIsr1, FALLING);
+
+    pinMode(tilt, INPUT);
+  attachInterrupt(tilt, buttonIsr1, FALLING);
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void receiveEvent(int bytes) { 
+   x = Wire.read();
+}
+
+
+void buttonIsr1() {
+    //Get the current milliseconds
+    unsigned long currentMillis = millis();
+
+    //When the difference between the current millisevonds and the previous milliseconds the intterupt fired is bigger or equal to the interval the function can be executed
+    if (currentMillis - previousMillis >= interval) 
+    { 
+      previousMillis = currentMillis; //store the milliseconds when the function has been executed
+      state = !state;
+
+      //TODO: do something
+    }
+}
+
+
+void tiltIsr1() {
+    //Get the current milliseconds
+    unsigned long currentMillis = millis();
+
+    //When the difference between the current millisevonds and the previous milliseconds the intterupt fired is bigger or equal to the interval the function can be executed
+    if (currentMillis - previousMillis >= interval) 
+    { 
+      previousMillis = currentMillis; //store the milliseconds when the function has been executed
+      state = !state;
+      //TODO: do something
+    }
 }
